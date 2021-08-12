07b9c3dcd8ecac4f203ea5b3cdb7f1957197fe56
Ávilla Silva <silvaavilla74@gmail.com>
2021-08-12 19:55:40 -0300

    notification using events

--- src/main/java/br/inatel/quotationmanagement/service/StockService.java
+++ src/main/java/br/inatel/quotationmanagement/service/StockService.java
@@ -1,8 +1,19 @@
 package br.inatel.quotationmanagement.service;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.Cacheable;
+import org.springframework.context.event.EventListener;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
@@ -31,4 +42,22 @@ public class StockService {
 		String url = API_URL + "/stock/" + stockId;
 		return restTemplate.getForObject(url, Stock.class);
 	}
+
+	@EventListener(ApplicationReadyEvent.class)
+    public void registerApplication() throws JsonProcessingException {
+        RestTemplate restTemplate = new RestTemplate();
+		HttpHeaders headers = new HttpHeaders();
+		headers.setContentType(MediaType.APPLICATION_JSON);
+        
+        Map<String, String> notification = new HashMap<String, String>();
+        notification.put("host", "localhost");
+        notification.put("port", String.valueOf(serverPort));
+
+        String jsonString = new ObjectMapper().writeValueAsString(notification);
+
+		HttpEntity<String> request = new HttpEntity<String>(jsonString, headers);
+        String response = restTemplate.postForObject(API_URL +  "/notification", request, String.class);
+
+        System.out.println("Registering application event... Response received: " + response);
+    }
 }
